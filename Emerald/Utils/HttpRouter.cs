6ae1346d452a9230a7fb457b0b8ef96344957dc9
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-02-04 21:28:04 +0300

    Adding http router.

---
+++ Emerald/Utils/HttpRouter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emerald.Utils
+{
+    public sealed class HttpRouter
+    {
+        private readonly string _routeTo;
+
+        public HttpRouter(string routeTo)
+        {
+            _routeTo = routeTo;
+        }
+
+        public async Task<HttpResponseMessage> Route(HttpRequestMessage httpRequestMessage)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var requestUrl = $"{_routeTo}{httpRequestMessage.RequestUri.GetComponents(UriComponents.PathAndQuery, UriFormat.UriEscaped)}";
+                var requestMessage = new HttpRequestMessage(httpRequestMessage.Method, requestUrl);
+
+                if (httpRequestMessage.Method != HttpMethod.Get)
+                {
+                    requestMessage.Content = new StringContent(await httpRequestMessage.Content.ReadAsStringAsync(), Encoding.UTF8, "application/json");
+                }
+
+                return await httpClient.SendAsync(requestMessage);
+            }
+        }
+    }
+}
