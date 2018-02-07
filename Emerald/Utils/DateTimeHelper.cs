c89d4103737f2b02a6ee7fdce60a5a215ec78eed
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-02-07 16:34:48 +0300

    Adding method for calculate week dates.

--- Emerald/Utils/DateTimeHelper.cs
+++ Emerald/Utils/DateTimeHelper.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Emerald.Utils
 {
     public static class DateTimeHelper
     {
-        public static DateTime? GetUtcDateTimeFromUnixTimeMilliseconds(long? milliseconds)
+        public static DateTime GetUtcDateTimeFromUnixTimeMilliseconds(long milliseconds)
         {
-            return milliseconds.HasValue ? DateTimeOffset.FromUnixTimeMilliseconds(milliseconds.Value).UtcDateTime : (DateTime?)null;
+            return DateTimeOffset.FromUnixTimeMilliseconds(milliseconds).UtcDateTime;
         }
 
         public static long ToUnixTimeMilliseconds(this DateTime dateTime)
         {
             return new DateTimeOffset(dateTime, TimeSpan.Zero).ToUnixTimeMilliseconds();
+        }
+
+        public static IEnumerable<DateTime> GetWeekDates(DateTime weekDate, DayOfWeek startDayOfWeek)
+        {
+            var startDateOfWeek = weekDate.AddDays(-1 * ((7 + (weekDate.DayOfWeek - startDayOfWeek)) % 7)).Date;
+            var weekDateList = new List<DateTime>(7) { startDateOfWeek };
+
+            for (var i = 1; i < 7; i++)
+            {
+                weekDateList.Add(startDateOfWeek.AddDays(i));
+            }
+
+            return weekDateList;
         }
     }
 }
