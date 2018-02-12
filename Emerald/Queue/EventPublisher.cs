85df0dd5b078fe2e33428d8d6e9a25f9b68386d5
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-02-12 22:51:28 +0300

    Adding possibility to publish events without emerald system build.

--- Emerald/Queue/EventPublisher.cs
+++ Emerald/Queue/EventPublisher.cs
@@ -7,14 +7,28 @@ namespace Emerald.Queue
 {
     public sealed class EventPublisher
     {
+        private readonly QueueDbAccessManager _queueDbAccessManager;
+
         internal EventPublisher()
         {
+        }
+        internal EventPublisher(QueueDbAccessManager queueDbAccessManager)
+        {
+            _queueDbAccessManager = queueDbAccessManager;
         }
 
         public async Task Publish(object @event)
         {
             if (@event == null) throw new ArgumentNullException(nameof(@event));
-            await Registry.QueueDbAccessManager.AddEvent(@event.GetType().FullName, JsonConvert.SerializeObject(@event));
+            var queueDbAccessManager = _queueDbAccessManager ?? Registry.QueueDbAccessManager;
+            await queueDbAccessManager.AddEvent(@event.GetType().FullName, JsonConvert.SerializeObject(@event));
+        }
+
+        public EventPublisher Create(string applicationName, string connectionString)
+        {
+            var queueDbAccessManager = new QueueDbAccessManager(applicationName, connectionString);
+            queueDbAccessManager.CreateQueueDbIfNeeded();
+            return new EventPublisher(queueDbAccessManager);
         }
     }
 }
