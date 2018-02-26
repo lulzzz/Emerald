fded76fd864ae6f0decb737c6e2abd6617ac3ba0
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-02-26 12:42:04 +0300

    Adding date time extension methods for convert to utc with specific offset.

--- Emerald/Utils/DateTimeHelper.cs
+++ Emerald/Utils/DateTimeHelper.cs
@@ -19,6 +19,27 @@ namespace Emerald.Utils
             return new DateTimeOffset(dateTime, TimeSpan.Zero).ToUnixTimeMilliseconds();
         }
 
+        /// <summary>Convert date time with specific offset to utc date time.</summary>
+        /// <param name="dateTime">Date time.</param>
+        /// <param name="offset">Offset of current date time.</param>
+        /// <returns>Utc date time.</returns>
+        public static DateTime ToUtcDateTime(this DateTime dateTime, TimeSpan offset)
+        {
+            var localDateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
+            localDateTime.AddMilliseconds(dateTime.Millisecond);
+            return new DateTimeOffset(localDateTime, offset).UtcDateTime;
+        }
+        /// <summary>Convert date time with specific offset to utc date time.</summary>
+        /// <param name="dateTime">Date time.</param>
+        /// <param name="offset">Offset of current date time in minutes.</param>
+        /// <returns>Utc date time.</returns>
+        public static DateTime ToUtcDateTime(this DateTime dateTime, double offset)
+        {
+            var localDateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
+            localDateTime.AddMilliseconds(dateTime.Millisecond);
+            return new DateTimeOffset(localDateTime, TimeSpan.FromMinutes(offset)).UtcDateTime;
+        }
+
         public static IEnumerable<DateTime> GetWeekDates(DateTime weekDate, DayOfWeek startDayOfWeek)
         {
             var startDateOfWeek = weekDate.AddDays(-1 * ((7 + (weekDate.DayOfWeek - startDayOfWeek)) % 7)).Date;
