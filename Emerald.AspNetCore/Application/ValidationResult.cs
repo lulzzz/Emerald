9f0f5bc620fdec6068ad5e30ee88a554a3b24153
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-03-14 14:28:33 +0300

    Adding generic version of validation result type.

--- Emerald.AspNetCore/Application/ValidationResult.cs
+++ Emerald.AspNetCore/Application/ValidationResult.cs
@@ -14,4 +14,21 @@
         public static ValidationResult Success() => new ValidationResult(ValidationResultType.Success, null);
         public static ValidationResult Error(string errorMessage) => new ValidationResult(ValidationResultType.Error, errorMessage);
     }
+
+    public sealed class ValidationResult<TOutput> where TOutput : class
+    {
+        private ValidationResult(ValidationResultType type, string errorMessage, TOutput output)
+        {
+            Type = type;
+            ErrorMessage = errorMessage;
+            Output = output;
+        }
+
+        public ValidationResultType Type { get; }
+        public string ErrorMessage { get; }
+        public TOutput Output { get; }
+
+        public static ValidationResult<TOutput> Success(TOutput output) => new ValidationResult<TOutput>(ValidationResultType.Success, null, output);
+        public static ValidationResult<TOutput> Error(string errorMessage) => new ValidationResult<TOutput>(ValidationResultType.Error, errorMessage, null);
+    }
 }
