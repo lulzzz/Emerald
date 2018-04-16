81859b851301719f4eef927a959b3d7b38c03970
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-04-16 19:52:01 +0300

    Adding some int32 extension methods.

---
+++ Emerald/Utils/Int32Extension.cs
@@ -0,0 +1,22 @@
+﻿namespace Emerald.Utils
+{
+    public static class Int32Extension
+    {
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+        public static bool IsGreaterThanOrEqual(this int i, int value)
+        {
+            return i >= value;
+        }
+        public static bool IsLessThan(this int i, int value)
+        {
+            return i < value;
+        }
+        public static bool IsLessThanOrEqual(this int i, int value)
+        {
+            return i <= value;
+        }
+    }
+}
