a16ad61c210e96f9fd97e0d57106242e5ec02349
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-04-18 18:14:26 +0300

    Adding extension methods.

--- Emerald/Utils/DateTimeExtension.cs
+++ Emerald/Utils/DateTimeExtension.cs
@@ -4,34 +4,16 @@ namespace Emerald.Utils
 {
     public static class DateTimeExtension
     {
-        public static long ToUnixTimeSeconds(this DateTime dateTime)
-        {
-            return new DateTimeOffset(dateTime, TimeSpan.Zero).ToUnixTimeSeconds();
-        }
         public static long ToUnixTimeMilliseconds(this DateTime dateTime)
         {
             return new DateTimeOffset(dateTime, TimeSpan.Zero).ToUnixTimeMilliseconds();
         }
-
-        /// <summary>Convert date time with specific offset to utc date time.</summary>
-        /// <param name="dateTime">Date time.</param>
-        /// <param name="offset">Offset of current date time.</param>
-        /// <returns>Utc date time.</returns>
-        public static DateTime ToUtcDateTime(this DateTime dateTime, TimeSpan offset)
+        public static DateTime[] GetWeekDates(this DateTime dateTime, DayOfWeek startDayOfWeek)
         {
-            var localDateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
-            localDateTime.AddMilliseconds(dateTime.Millisecond);
-            return new DateTimeOffset(localDateTime, offset).UtcDateTime;
-        }
-        /// <summary>Convert utc date time with specific offset to utc date time.</summary>
-        /// <param name="dateTime">Date time.</param>
-        /// <param name="offset">Offset of current date time in minutes.</param>
-        /// <returns>Utc date time.</returns>
-        public static DateTime ToUtcDateTime(this DateTime dateTime, double offset)
-        {
-            var localDateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
-            localDateTime.AddMilliseconds(dateTime.Millisecond);
-            return new DateTimeOffset(localDateTime, TimeSpan.FromMinutes(offset)).UtcDateTime;
+            var weekDateArray = new DateTime[7];
+            weekDateArray[0] = dateTime.AddDays(-1 * ((7 + (dateTime.DayOfWeek - startDayOfWeek)) % 7)).Date;
+            for (var i = 1; i < 7; i++) weekDateArray[i] = weekDateArray[0].AddDays(i);
+            return weekDateArray;
         }
     }
 }
