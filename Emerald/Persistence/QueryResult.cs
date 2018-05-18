3d2e6032f444cf49d749f09d89f8fee837b6c23a
dmitry.sidorov <dmitry.sidorov@outlook.com>
2018-05-18 14:33:14 +0300

    Improving query result.

--- Emerald/Persistence/QueryResult.cs
+++ Emerald/Persistence/QueryResult.cs
@@ -20,6 +20,20 @@
         public static QueryResult<TOutput> Success(TOutput output) => new QueryResult<TOutput>(QueryResultType.Success, null, output);
         public static QueryResult<TOutput> NotFound() => new QueryResult<TOutput>(QueryResultType.NotFound, null, default(TOutput));
         public static QueryResult<TOutput> Error(string errorMessage) => new QueryResult<TOutput>(QueryResultType.Error, errorMessage, default(TOutput));
-        public static QueryResult<byte[]> File(byte[] output) => new QueryResult<byte[]>(QueryResultType.File, null, output);
+        public static QueryResult<QueryResultFileOutput> File(QueryResultFileOutput output) => new QueryResult<QueryResultFileOutput>(QueryResultType.File, null, output);
+    }
+
+    public sealed class QueryResultFileOutput
+    {
+        public QueryResultFileOutput(byte[] content, string contentType, string fileName)
+        {
+            Content = content;
+            ContentType = contentType;
+            FileName = fileName;
+        }
+
+        public byte[] Content { get; }
+        public string ContentType { get; }
+        public string FileName { get; }
     }
 }
